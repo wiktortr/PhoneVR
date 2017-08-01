236ed1841a311c0284acdaae91be0ad99409d3f4
wiktortr <wiktortr9@gmail.com>
2017-08-01 09:02:08 +0200

    Test OpenGL and LZ4

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -3,94 +3,117 @@
 
 #include "net.hpp"
 
+#pragma comment(lib,"opengl32.lib")
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+#include <lz4.h>
+
 using namespace std;
 
-#define CHUNK_SIZE 10
+int width = 800;
+int height = 600;
 
 int main() {
 
-	if (NetInit() != NET_OK)
-		cout << "Error init net" << endl;
+	if (!glfwInit())
+		cout << "Error init glfw" << endl;
 
-	//Create main socket
-	NetSocket mainSocket = NetSocketInit(NET_PROTO_TCP);
-	if (mainSocket == NET_INVALID_SOCKET) {
-		cout << "Error creating socket: " << WSAGetLastError() << endl;
-		WSACleanup();
-	}
-	else
-		cout << "Socket Created ok" << endl;
+	GLFWwindow* win = glfwCreateWindow(width, height, "PhoneVR server", NULL, NULL);
+	glfwMakeContextCurrent(win);
 
+	if (GLenum err = glewInit() != GLEW_OK)
+		cout << "GLEW init error: " << (char*)glewGetErrorString(err) << endl;
 
-	//Create service
-	NetSocketAddr service = NetInitSocketAddr("127.0.0.1", 27015);
+	while (!glfwWindowShouldClose(win)){
+		glClearColor(0.25, 0.25, 0.60, 1.00);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glViewport(0, 0, width, height);
 
+		unsigned char* color_buffer = new unsigned char[3 * width * height];
+		unsigned int buffer_size = 3 * width*height;
+		glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, color_buffer);
 
-	//Bind soocket and service
-	if (NetBind(mainSocket, service) == NET_ERROR) {
-		cout << "Error bind socket" << endl;
-		closesocket(mainSocket);
-	}
-	else
-		cout << "Bind ok" << endl;
+		//for (int i = 0; i < height; i++){
+		//	for (int j = 0; j < width; j++){
+		//		unsigned int pos = 3 * ((height - i - 1) * width + j);
+		//		cout << (double)color_buffer[pos + 2] << endl;
+		//		//cout << double(color_buffer[i + j]) << endl;
+		//	}
+		//}
 
+		cout << buffer_size << endl;
+		cout << LZ4_compressBound(buffer_size) << endl;
 
-	//Listen
-	if (NetListen(mainSocket) == NET_ERROR)
-		cout << "Error listening on socket" << endl;
-	else
-		cout << "ok listen on socket" << endl;
+		char* out_put = new char[LZ4_compressBound(buffer_size)];
+		int byte_return = LZ4_compress_default((char*)color_buffer, out_put, buffer_size, LZ4_compressBound(buffer_size));
 
+		cout << byte_return << endl;
 
-	//waiting for client
-	SOCKET acceptSocket = NET_ERROR;
-	cout << "Waiting for a client to connect..." << endl;
-
-	while (acceptSocket == NET_ERROR) {
-		acceptSocket = NetAccept(mainSocket);
+		system("pause");
+		glfwPollEvents();
+		glfwSwapBuffers(win);
 	}
 
+	glfwDestroyWindow(win);
+	glfwTerminate();
 
-	cout << "Client connected" << endl;
-	mainSocket = acceptSocket;
+	////Init net socket
+	//if (NetInit() != NET_OK)
+	//	cout << "Error init net" << endl;
 
-
-	////connect client to server
-	//if (connect(mainSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
-	//	cout << "Failed to connect" << endl;
+	////Create main socket
+	//NetSocket mainSocket = NetSocketInit(NET_PROTO_TCP);
+	//if (mainSocket == NET_INVALID_SOCKET) {
+	//	cout << "Error creating socket: " << WSAGetLastError() << endl;
 	//	WSACleanup();
 	//}
 	//else
-	//	cout << "connect ok" << endl;
+	//	cout << "Socket Created ok" << endl;
 
 
-	//Send data
-	for (int i = 0; i < 1000; i++){
-		string message = to_string(i) + ". Data send";
-		int out = NetWrite(mainSocket, message.c_str(), message.size());
-		cout << out << endl;
-		//	cout << "Error send data" << endl;
-	}
-	
+	////Create service
+	//NetSocketAddr service = NetInitSocketAddr("127.0.0.1", 27015);
 
 
-	//Recive data
-	//string rev_data = "";
-	//int size_recv, total_size = 0;
-	//char chunk[CHUNK_SIZE];
-	//while (1){
-	//	memset(chunk, 0, CHUNK_SIZE);  //clear the variable
-	//	if ((size_recv = recv(mainSocket, chunk, CHUNK_SIZE, 0)) < 0)
-	//		break;
-	//	else{
-	//		total_size += size_recv;
-	//		chunk[size_recv] = '\0';
-	//		rev_data += chunk;
-	//	}
+	////Bind soocket and service
+	//if (NetBind(mainSocket, service) == NET_ERROR) {
+	//	cout << "Error bind socket" << endl;
+	//	closesocket(mainSocket);
+	//}
+	//else
+	//	cout << "Bind ok" << endl;
+
+
+	////Listen
+	//if (NetListen(mainSocket) == NET_ERROR)
+	//	cout << "Error listening on socket" << endl;
+	//else
+	//	cout << "ok listen on socket" << endl;
+
+
+	////waiting for client
+	//SOCKET acceptSocket = NET_ERROR;
+	//cout << "Waiting for a client to connect..." << endl;
+
+	//while (acceptSocket == NET_ERROR) {
+	//	acceptSocket = NetAccept(mainSocket);
 	//}
 
-	//cout << rev_data << endl;
 
-	system("pause");
+	//cout << "Client connected" << endl;
+	//mainSocket = acceptSocket;
+
+
+
+	////Send data
+	//for (int i = 0; i < 1000; i++){
+	//	string message = to_string(i) + ". Data send";
+	//	int out = NetWrite(mainSocket, message.c_str(), message.size());
+	//	cout << out << endl;
+	//}
+
+	//NetDeInit();
+
 	return 0;
 }
