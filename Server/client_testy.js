da834c17e60f5e80363f8a73f6ca226ea0b9f8f1
wiktortr <wiktortr9@gmail.com>
2017-08-04 08:19:32 +0200

    Update

--- Server/client_testy.js
+++ Server/client_testy.js
@@ -1,23 +1,21 @@
 var net = require('net');
-var lz4 = require("lz4js");
 
 
 var client = new net.Socket();
-client.connect(27015, '127.0.0.1', function() {
+client.connect(27015, '192.168.1.13', function() {
     console.log('Connected');
-});
+    var p_t, a_t = 0;
 
-var p_t, a_t = 0;
-
-client.on('data', function(data) {
-    p_t = a_t;
-    a_t = Date.now().valueOf();
-    var delta_t = a_t - p_t;
-    // console.log("Received: " + data + " time_delta: " + delta_t + "\n");
-    //console.log(client.bufferSize);
-    console.log(data.length);
-    //console.log(lz4.decompress(data).length);
-    //client.destroy();
+    client.on('data', function(data) {
+        p_t = a_t;
+        a_t = Date.now().valueOf();
+        var delta_t = a_t - p_t;
+        // console.log("Received: " + data + " time_delta: " + delta_t + "\n");
+        //console.log(client.bufferSize);
+        console.log(data.length + "    time: " + delta_t);
+        //console.log(lz4.decompress(data).length);
+        //client.destroy();
+    });
 });
 
 // client.once("data",function(d){
@@ -25,7 +23,7 @@ client.on('data', function(data) {
 //     //client.destroy();
 // });
 
-client.on('close', function() {
-    console.log('Connection closed');
-    client.destroy();
-});
+// client.on('close', function() {
+//     console.log('Connection closed');
+//     client.destroy();
+// });
